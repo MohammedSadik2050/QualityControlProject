ff31d43a0b42f2902f31d201fea321a5ac61874d
mohammed sadik <Mohammed_Sadik@live.com>
2023-02-04 19:14:25 +0200

    0

--- aspnet-core/src/eConLab.Core/Authorization/PermissionNames.cs
+++ aspnet-core/src/eConLab.Core/Authorization/PermissionNames.cs
@@ -3,10 +3,10 @@
     public static class PermissionNames
     {
         public const string Pages_Tenants = "Pages.Tenants";
-
         public const string Pages_Users = "Pages.Users";
+        public const string Pages_Manage_Agences = "Pages.Manage.Agences";
+        public const string Pages_Manage_Project = "Pages.Manage.Project";
         public const string Pages_Users_Activation = "Pages.Users.Activation";
-
         public const string Pages_Roles = "Pages.Roles";
     }
 }
