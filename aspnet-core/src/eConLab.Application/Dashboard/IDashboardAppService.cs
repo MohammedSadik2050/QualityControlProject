081e522c8fff0457be8330ce78c36e611fef9f71
mohammed sadik <Mohammed_Sadik@live.com>
2023-03-21 19:55:07 +0200

    000

---
+++ aspnet-core/src/eConLab.Application/Dashboard/IDashboardAppService.cs
@@ -0,0 +1,16 @@
+﻿using Abp.Application.Services;
+using eConLab.Dashboard.Dto;
+using eConLab.QCUsers.Dto;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace eConLab.Dashboard
+{
+    public interface IDashboardAppService : IApplicationService
+    {
+        Task<StatisticsDto> GetSystemStatisticsDashboard();
+    }
+}
